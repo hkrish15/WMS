6dc94dc01b601e240e179c2f3b27f7c4d2e1a101
Hari Krishna <83591942+hkrish15@users.noreply.github.com>
2025-04-29 09:39:05 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tailwind scans these files for class names
+    ],
+    theme: {
+    extend: {
+    // Customize your theme here if needed
+    },
+    },
+    plugins: [],
+    };
